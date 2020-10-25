e7404806941a2912f50e7ad0925339871074cd92
salvadorf8 <aztecsalvador@gmail.com>
2020-10-24 21:39:03 -0500

    removed notes for next course

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-// react gives every html markup elements - style, takes an object that has prop values
-// we are using javascript template strings here to display image in background (string terpulation)
-// also string interpolation classname to get the size that were added on the object since image is bigger
 const MenuItem = ({ title, imageUrl, size }) => (
     <div className={`${size} menu-item`}>
-        {/* added below div so the animation will work when hovering over - also by itself so box wont resize */}
         <div className='background-image' style={{ backgroundImage: `url(${imageUrl}` }} />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
